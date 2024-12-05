018f45c3e384eedb8ac8684d1fdfa2f6d9f4fb8f
ahgr3y <leehh97@gmail.com>
2024-12-05 15:46:00 +0800

    Add functions for basic math operators

--- javascript.js
+++ javascript.js
@@ -1,2 +1,18 @@
 const div = document.querySelector("div");
 div.textContent = "Hello World!";
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
