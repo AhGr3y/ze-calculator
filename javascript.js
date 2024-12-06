98a20e2c4bc79f68a0a45ab7c54b77b7e3819365
ahgr3y <leehh97@gmail.com>
2024-12-06 14:39:40 +0800

    Add utility functions

--- javascript.js
+++ javascript.js
@@ -1,6 +1,11 @@
+const operators = ["+", "-", "*", "/"];
+
 let numA;
 let numB;
 let operator;
+let displayContent = document.querySelector("#display-content");
+
+updateDisplay(displayContent, 0);
 
 function add(a, b) {
     return a + b;
@@ -18,20 +23,66 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, a, b) {
+function extractOperator(input) {
+    let operator = operators.filter(operator => input.includes(operator))[0];
+    if (!operator) {
+        return "";
+    }
+    return operator;
+}
+
+function extractInputParts(input) {
+    let operator = extractOperator(input);
+    if (operator === "") {
+        return [input];
+    }
+    let operands = input.split(operator);
+    operands.splice(1, 0, operator);
+    return operands;
+}
+
+function updateDisplay(display, text) {
+    display.textContent = text;
+}
+
+function operate(input) {
+    const inputParts = extractInputParts(input);
+
+    let a = Number(inputParts[0]);
+    let b;
+    let operator;
+
+    if (inputParts.length === 1) {
+        updateDisplay(displayContent, a);
+        return;
+    }
+
+    if (inputParts.length > 1) {
+        operator = inputParts[1];
+        b = Number(inputParts[2]);
+    }
+
     switch(operator) {
         case "+":
-            return add(a, b);
+            updateDisplay(displayContent, add(a, b));
+            return;
         case "-":
-            return subtract(a, b);
+            updateDisplay(displayContent, subtract(a, b));
+            return;
         case "*":
-            return multiply(a, b);
+            updateDisplay(displayContent, multiply(a, b));
+            return;
         case "/":
             if (b === 0) {
-                return "baka";
+                updateDisplay(displayContent, "baka");
+                return;
             }
-            return divide(a, b);
+            updateDisplay(displayContent, divide(a, b));
+            return;
         default:
-            return "err";
+            updateDisplay(displayContent, "err");
+            return;
     }
 }
+
+
