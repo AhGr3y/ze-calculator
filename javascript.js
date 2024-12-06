293bdc942b88f5527d16a330512e46c998499570
ahgr3y <leehh97@gmail.com>
2024-12-06 16:14:39 +0800

    Make button update display

--- javascript.js
+++ javascript.js
@@ -3,9 +3,72 @@ const operators = ["+", "-", "*", "/"];
 let numA;
 let numB;
 let operator;
+
 let displayContent = document.querySelector("#display-content");
 
-updateDisplay(displayContent, 0);
+const one = document.querySelector("#one");
+one.addEventListener("click", (e) => {
+    appendDisplay(one.value);
+});
+
+const two = document.querySelector("#two");
+two.addEventListener("click", (e) => {
+    appendDisplay(two.value);
+});
+
+const three = document.querySelector("#three");
+three.addEventListener("click", (e) => {
+    appendDisplay(three.value);
+});
+
+const four = document.querySelector("#four");
+four.addEventListener("click", (e) => {
+    appendDisplay(four.value);
+});
+
+const five = document.querySelector("#five");
+five.addEventListener("click", (e) => {
+    appendDisplay(five.value);
+});
+
+const six = document.querySelector("#six");
+six.addEventListener("click", (e) => {
+    appendDisplay(six.value);
+});
+
+const seven = document.querySelector("#seven");
+seven.addEventListener("click", (e) => {
+    appendDisplay(seven.value);
+});
+
+const eight = document.querySelector("#eight");
+eight.addEventListener("click", (e) => {
+    appendDisplay(eight.value);
+});
+
+const nine = document.querySelector("#nine");
+nine.addEventListener("click", (e) => {
+    appendDisplay(nine.value);
+});
+
+const period = document.querySelector("#period");
+period.addEventListener("click", (e) => {
+    if (!String(displayContent.textContent).includes(".")) {
+        appendDisplay(period.value);
+    }
+});
+
+const ac = document.querySelector("#clear");
+ac.addEventListener("click", (e) => {
+    updateDisplay("0");
+});
+
+const del = document.querySelector("#delete");
+del.addEventListener("click", (e) => {
+    deleteDisplayFromRight();
+});
+
+updateDisplay("0");
 
 function add(a, b) {
     return a + b;
@@ -41,8 +104,35 @@ function extractInputParts(input) {
     return operands;
 }
 
-function updateDisplay(display, text) {
-    display.textContent = text;
+function updateDisplay(data) {
+    if (typeof data === "number" && isFloat(data)) {
+        displayContent.textContent = data.toFixed(4);
+        return
+    }
+
+    displayContent.textContent = data;
+}
+
+function appendDisplay(data) {
+    if (displayContent.textContent === "0") {
+        updateDisplay(data);
+        return;
+    }
+    displayContent.textContent += data;
+    return;
+}
+
+function deleteDisplayFromRight() {
+    let displayString = String(displayContent.textContent);
+    if (displayString.length > 1) {
+        updateDisplay(displayString.slice(0, -1));
+    } else if (displayString.length === 1) {
+        updateDisplay("0");
+    }
+}
+
+function isFloat(num) {
+    return num % 1 !== 0;
 }
 
 function operate(input) {
@@ -64,25 +154,23 @@ function operate(input) {
 
     switch(operator) {
         case "+":
-            updateDisplay(displayContent, add(a, b));
+            updateDisplay(add(a, b));
             return;
         case "-":
-            updateDisplay(displayContent, subtract(a, b));
+            updateDisplay(subtract(a, b));
             return;
         case "*":
-            updateDisplay(displayContent, multiply(a, b));
+            updateDisplay(multiply(a, b));
             return;
         case "/":
             if (b === 0) {
-                updateDisplay(displayContent, "baka");
+                updateDisplay("baka");
                 return;
             }
-            updateDisplay(displayContent, divide(a, b));
+            updateDisplay(divide(a, b));
             return;
         default:
-            updateDisplay(displayContent, "err");
+            updateDisplay("err");
             return;
     }
 }
-
-
